f5c9b6c10da743da7f0ff1bee3990aaaa0ee0c64
Oriol Angelet <uriaeri@gmail.com>
2023-02-04 12:21:04 +0100

    first commit

---
+++ backendApp/backendApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  backendApp
+//
+//  Created by Uri on 4/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
